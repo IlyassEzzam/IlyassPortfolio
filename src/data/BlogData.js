7b1ccf7e0e099d89023bf107ae735928364e121b
KANEKI <110631781+KanekiEzzam@users.noreply.github.com>
2023-10-26 18:59:30 +0200

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,67 @@
+
+export const Blogs = [
+    {
+        id:1,
+        name:"My page on GitHup where I post my projects",
+        tags:["react.js","javaScript","html", "css ..."],
+        date:"13 May, 2022",
+        imgSrc:require("../assets/Images/Blog/githup.png"),
+        link:"https://github.com/IlyassEzzam",
+    },
+    {
+        id:2,
+        name:"Landing page for a store project with many incomplete pages",
+        tags:["react.js","html" , "css"],
+        date:"5 July, 2023",
+        imgSrc:require("../assets/Images/Blog/ecomerce.png"),
+        link:"https://ilyassezzam.github.io/teamplate-ecomerce/indesx.html",
+    },
+    {
+        id:3,
+        name:"Landing page for a food store project",
+        tags:["css","html","bootstrap", "js"],
+        date:"6 July, 2023",
+        imgSrc: require("../assets/Images/Blog/food.png"),
+        link: "https://foodkanekii.000webhostapp.com/",
+    },
+    {
+        id:4,
+        name:"A site for using audio and songs, Spotify",
+        tags:["css","html","talwindcss", "js"],
+        date:"5 July, 2023",
+        imgSrc:require("../assets/Images/Blog/podmix.png"),
+        link:"https://ilyassezzam.github.io/Podmmix/",
+    },
+    {
+        id:5,
+        name:"Beauty gallery landing page",
+        tags:["css","html", "js"],
+        date:"3 Feb, 2023",
+        imgSrc:require("../assets/Images/Blog/Carlos.png"),
+        link:"https://ilyassezzam.github.io/templat-carlos/"
+    },
+    {
+        id:6,
+        name:"Landing page to log in and create an account",
+        tags:["react","css", "html"],
+        date:"5 Jan, 2023",
+        imgSrc:require("../assets/Images/Blog/signIn.png"),
+        link:"https://ilyassezzam.github.io/SignIn/"
+    },
+    {
+        id:7,
+        name:"New generation payment website",
+        tags:["react","redux","javascript", "tailwindCss"],
+        date:"16 May, 2023",
+        imgSrc:require("../assets/Images/Blog/vitePayment.png"),
+        link:"https://ilyassezzam.github.io/vite-project/"
+    },
+    {
+        id:8,
+        name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+        tags:["react","framer-motion","styled-components"],
+        date:"26 May, 2023",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+        link:"https://react-sidebar.vercel.app/calender",
+    },
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,68 @@
+export const Work = [
+    {
+        id:1,
+        name:"My page on GitHup where I post my projects",
+        description:"It is a website where all the projects I am working on are located.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://github.com/IlyassEzzam",
+        github:"https://github.com/IlyassEzzam"
+    },
+    {
+        id:2,
+        name:"E-commerce website",
+        description:"Landing page for a store project with many incomplete pages.",
+        tags:["react","sass"],
+        
+        demo:"https://ilyassezzam.github.io/teamplate-ecomerce/indesx.html",
+        github:"https://github.com/IlyassEzzam/teamplate-ecomerce"
+        
+    },
+    {
+        id:3,
+        name:"Food Kaneki Website",
+        description:"Landing page for a food store project.",
+        tags:["react","api","sass"],
+        
+        demo:"https://foodkanekii.000webhostapp.com/",
+        github:"https://github.com/IlyassEzzam/templat-carlos/"
+    },
+    {
+        id:4,
+        name:"podcast Website",
+        description:"A site for using audio and songs, Spotify.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://ilyassezzam.github.io/Podmmix/",
+        github:"https://github.com/IlyassEzzam/Podmmix"
+    },{
+        id:5,
+        name:"carlos web site",
+        description:"Beauty gallery landing page.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"https://ilyassezzam.github.io/templat-carlos/",
+        github:"https://github.com/IlyassEzzam/templat-carlos"
+        
+    },
+    {
+        id:6,
+        name:"Sign in",
+        description:"Project is a login page.",
+        tags:["react","react-router","css"],
+        
+        demo:"https://ilyassezzam.github.io/SignIn/",
+        github:"https://github.com/IlyassEzzam/Sign._.In"
+        
+    },
+    {
+        id:7,
+        name:"vite_payment",
+        description:"Project is a Payment page.",
+        tags:["react","Vite-react", "tailwinds"],
+        
+        demo:"https://ilyassezzam.github.io/vite-project/",
+        github:"https://github.com/IlyassEzzam/vite-project"
+        
+    },
+] 
